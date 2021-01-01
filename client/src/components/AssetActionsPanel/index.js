99a1026e2c7623ade02fd9cae888b566cf153f2f
JamesLee <48775473+titanian229@users.noreply.github.com>
2021-01-01 15:51:20 -0500

    Added asset action panels with buttons with text label for desktop size

--- client/src/components/AssetActionsPanel/index.js
+++ client/src/components/AssetActionsPanel/index.js
@@ -2,35 +2,78 @@ import React from 'react';
 import {
     IconButton,
     // Tooltip,
-    Grid
+    Grid,
+    Button,
 } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
 import HighlightOffOutlinedIcon from '@material-ui/icons/HighlightOffOutlined';
 
+const useStyles = makeStyles((theme) => ({
+    button: {
+        margin: theme.spacing(1),
+        [theme.breakpoints.down('sm')]: {
+            display: 'none',
+        },
+    },
+    iconButton: {
+        [theme.breakpoints.up('sm')]: {
+            display: 'none',
+        },
+    },
+}));
+
 const AssetActionsPanel = (props) => {
     const { handleEdit, handleDelete, handleDissociate, asset, sideComponent } = props;
+    const classes = useStyles();
 
     return (
         <Grid container justify="space-evenly">
             {handleEdit && (
-                <IconButton onClick={() => handleEdit(asset)}>
-                    <EditOutlinedIcon />
-                </IconButton>
+                <>
+                    <IconButton className={classes.iconButton} onClick={() => handleEdit(asset)}>
+                        <EditOutlinedIcon />
+                    </IconButton>
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        className={classes.button}
+                        startIcon={<EditOutlinedIcon />}
+                    >
+                        Edit
+                    </Button>
+                </>
             )}
             {handleDissociate && (
-                <IconButton onClick={() => handleDissociate(asset)}>
-                    <HighlightOffOutlinedIcon />
-                </IconButton>
+                <>
+                    <IconButton className={classes.iconButton} onClick={() => handleDissociate(asset)}>
+                        <HighlightOffOutlinedIcon />
+                    </IconButton>
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        className={classes.button}
+                        startIcon={<HighlightOffOutlinedIcon />}
+                    >
+                        Remove
+                    </Button>
+                </>
             )}
             {handleDelete && (
-                <IconButton onClick={handleDelete}>
-                    <DeleteIcon />
-                </IconButton>
+                <>
+                    <IconButton className={classes.iconButton} onClick={handleDelete}>
+                        <DeleteIcon />
+                    </IconButton>
+                    <Button variant="contained" color="delete" className={classes.button} startIcon={<DeleteIcon />}>
+                        Delete
+                    </Button>
+                </>
             )}
+
             {sideComponent}
         </Grid>
     );
 };
 
-export default AssetActionsPanel
+export default AssetActionsPanel;
