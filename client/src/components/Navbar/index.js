a976703675b9a677053ccef789ebd997e7f74d3a
JamesLee <48775473+titanian229@users.noreply.github.com>
2021-01-11 17:53:33 -0500

    Changed desktop version's navbar icons based on user login status

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -18,7 +18,7 @@ import AddAlertIcon from '@material-ui/icons/AddAlertTwoTone';
 import WorkIcon from '@material-ui/icons/WorkTwoTone';
 import SettingsIcon from '@material-ui/icons/SettingsApplicationsTwoTone';
 import ExitToAppIcon from '@material-ui/icons/ExitToAppTwoTone';
-// import LockOpenOutlinedIcon from '@material-ui/icons/LockOpenOutlined';
+import LockOpenOutlinedIcon from '@material-ui/icons/LockOpenOutlined';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -66,81 +66,98 @@ const Navbar = () => {
                     Application Assistant
                 </Typography>
                 {/* //icons visible on desktop view */}
-                <Tooltip title="Applications">
-                    <IconButton
-                        className={classes.desktopNavIcons}
-                        edge="start"
-                        color="inherit"
-                        aria-label="Applications"
-                        component={Link}
-                        to="/applications"
-                    >
-                        <WorkIcon />
-                    </IconButton>
-                </Tooltip>
-                <Tooltip title="Contacts">
-                    <IconButton
-                        className={classes.desktopNavIcons}
-                        edge="start"
-                        color="inherit"
-                        aria-label="Contacts"
-                        component={Link}
-                        to="/contacts"
-                    >
-                        <ContactsIcon />
-                    </IconButton>
-                </Tooltip>
-                <Tooltip title="Resumes">
-                    <IconButton
-                        className={classes.desktopNavIcons}
-                        edge="start"
-                        color="inherit"
-                        aria-label="Resumes"
-                        component={Link}
-                        to="/resumes"
-                    >
-                        <DescriptionIcon />
-                    </IconButton>
-                </Tooltip>
-                <Tooltip title="Todos and Reminders">
-                    <IconButton
-                        className={classes.desktopNavIcons}
-                        edge="start"
-                        color="inherit"
-                        aria-label="Todos and Reminders"
-                        component={Link}
-                        to="/todos"
-                    >
-                        <AddAlertIcon />
-                    </IconButton>
-                </Tooltip>
-                <Divider className={classes.divider} orientation="vertical" flexItem />
+                {globalStore.isAuthenticated ? (
+                    <>
+                        <Tooltip title="Applications">
+                            <IconButton
+                                className={classes.desktopNavIcons}
+                                edge="start"
+                                color="inherit"
+                                aria-label="Applications"
+                                component={Link}
+                                to="/applications"
+                            >
+                                <WorkIcon />
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="Contacts">
+                            <IconButton
+                                className={classes.desktopNavIcons}
+                                edge="start"
+                                color="inherit"
+                                aria-label="Contacts"
+                                component={Link}
+                                to="/contacts"
+                            >
+                                <ContactsIcon />
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="Resumes">
+                            <IconButton
+                                className={classes.desktopNavIcons}
+                                edge="start"
+                                color="inherit"
+                                aria-label="Resumes"
+                                component={Link}
+                                to="/resumes"
+                            >
+                                <DescriptionIcon />
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="Todos and Reminders">
+                            <IconButton
+                                className={classes.desktopNavIcons}
+                                edge="start"
+                                color="inherit"
+                                aria-label="Todos and Reminders"
+                                component={Link}
+                                to="/todos"
+                            >
+                                <AddAlertIcon />
+                            </IconButton>
+                        </Tooltip>
+                        <Divider className={classes.divider} orientation="vertical" flexItem />
 
-                <Tooltip title="Settings and Profile">
-                    <IconButton
-                        className={classes.desktopNavIcons}
-                        edge="start"
-                        color="inherit"
-                        aria-label="Settings and Profile"
-                        component={Link}
-                        to="/settings"
-                    >
-                        <SettingsIcon />
-                    </IconButton>
-                </Tooltip>
-                <Tooltip title="Log Out">
-                    <IconButton
-                        className={classes.desktopNavIcons}
-                        edge="start"
-                        color="inherit"
-                        aria-label="Log out"
-                        component={Link}
-                        to="/logout"
-                    >
-                        <ExitToAppIcon />
-                    </IconButton>
-                </Tooltip>
-                {/* //END DESKTOP BUTTONS */}
+                        <Tooltip title="Settings and Profile">
+                            <IconButton
+                                className={classes.desktopNavIcons}
+                                edge="start"
+                                color="inherit"
+                                aria-label="Settings and Profile"
+                                component={Link}
+                                to="/settings"
+                            >
+                                <SettingsIcon />
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="Log Out">
+                            <IconButton
+                                className={classes.desktopNavIcons}
+                                edge="start"
+                                color="inherit"
+                                aria-label="Log out"
+                                component={Link}
+                                to="/logout"
+                            >
+                                <ExitToAppIcon />
+                            </IconButton>
+                        </Tooltip>
+                        {/* //END DESKTOP BUTTONS */}
+                    </>
+                ) : (
+                    <Tooltip title="Log In">
+                        <IconButton
+                            className={classes.desktopNavIcons}
+                            edge="start"
+                            color="inherit"
+                            aria-label="Log In"
+                            component={Link}
+                            to="/login"
+                        >
+                            <LockOpenOutlinedIcon />
+                        </IconButton>
+                    </Tooltip>
+                )}
                 <IconButton
                     className={clsx(globalStore.reminders === 0 && classes.hidden)}
                     edge="start"
